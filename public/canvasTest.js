4cf4fa5c98197b11185ec62021130abb9a4cf7a2
Mike Mossey <michaelmossey@gmail.com>
2019-10-06 17:36:25 -0700

    thrashing

--- public/canvasTest.js
+++ public/canvasTest.js
@@ -1,14 +1,27 @@
+let canv = document.getElementById('canv');
+let ctx = canv.getContext('2d');
 function interp_3_2(x0, x1, x2, y0, y1) {
     if (x0 == x2)
         throw "error in interp_3_2";
     return y0 + (y1 - y0) * (x1 - x0) / (x2 - x0);
 }
-let canv = document.getElementById('canv');
-let ctx = canv.getContext('2d');
-let p = { n: 10, value: 15 };
-let x0 = 100.0;
-let x1 = 200.0;
-let x = interp_3_2(0, p.n, 20, x0, x1);
-let y = interp_3_2(-30, p.value, 30, 600, 300);
-ctx.fillRect(x, y, 10, 600 - y);
+ctx.fillRect(0, 600, 6, -200);
+function draw(arr) {
+    ctx.clearRect(0, 0, 600, 600);
+    for (let i = 0; i < arr.length; ++i) {
+        let xc = interp_3_2(0, i, arr.length, 0, 600);
+        let w = 600 / arr.length;
+        let yc = interp_3_2(0, arr[i], 600, 0, -600);
+        ctx.fillRect(xc, 600, w, yc);
+    }
+}
+function main() {
+    let arr = [];
+    for (let i = 0; i < 100; i++) {
+        arr.push(i);
+    }
+    draw(arr);
+}
+main();
+console.log("foo");
 //# sourceMappingURL=canvasTest.js.map
