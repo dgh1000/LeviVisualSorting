25d1114e72e2a01b1eed89b0dc3dbc3c7362e691
Levi Wallace <guysmiley.levi@gmail.com>
2019-10-06 17:35:10 -0700

    shuffe

--- public/canvasTest.js
+++ public/canvasTest.js
@@ -1,14 +1,39 @@
+let canv = document.getElementById('canv');
+let ctx = canv.getContext('2d');
 function interp_3_2(x0, x1, x2, y0, y1) {
     if (x0 == x2)
         throw "error in interp_3_2";
     return y0 + (y1 - y0) * (x1 - x0) / (x2 - x0);
 }
-let canv = document.getElementById('canv');
-let ctx = canv.getContext('2d');
-let p = { n: 10, value: 15 };
-let x0 = 100.0;
-let x1 = 200.0;
-let x = interp_3_2(0, p.n, 20, x0, x1);
-let y = interp_3_2(-30, p.value, 30, 600, 300);
-ctx.fillRect(x, y, 10, 600 - y);
+function swap(xs, i1, i2) {
+    let b = xs[i1];
+    xs[i1] = xs[i2];
+    xs[i2] = b;
+}
+function shuffle(xs) {
+    let out = xs;
+    let j;
+    for (let i = xs.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * i);
+        swap(out, i, j);
+    }
+    return out;
+}
+function main() {
+    let out = [];
+    for (let i = 0; i < 600; i++) {
+        out.push(i);
+    }
+    out = shuffle(out);
+    draw(out);
+}
+function draw(xs) {
+    for (let i = 0; i < xs.length; i++) {
+        let xc = interp_3_2(0, i, xs.length, 0, 600);
+        let w = 600 / xs.length;
+        let yc = interp_3_2(0, xs[i], xs.length, 0, -600);
+        ctx.fillRect(xc, 600, w, yc);
+    }
+}
+main();
 //# sourceMappingURL=canvasTest.js.map
--- src/canvasTest.ts
+++ src/canvasTest.ts
@@ -1,18 +1,48 @@
+let canv = document.getElementById('canv') as HTMLCanvasElement;
+let ctx = canv.getContext('2d');
+
 function interp_3_2(x0: number, x1: number, x2: number, y0: number, y1: number) {
     if (x0 == x2)
         throw "error in interp_3_2";
     return y0 + (y1 - y0)*(x1 - x0)/(x2 - x0);
 }
 
-let canv = document.getElementById('canv') as HTMLCanvasElement;
-let ctx = canv.getContext('2d');
+function swap(xs: number[], i1: number, i2: number): void {
+    let b = xs[i1];
+    xs[i1] = xs[i2];
+    xs[i2] = b;
+}
 
-let p = {n: 10, value: 15};
-let x0 = 100.0;
-let x1 = 200.0;
-let x = interp_3_2(0, p.n, 20, x0, x1);
-let y = interp_3_2(-30, p.value, 30, 600, 300);
-ctx.fillRect(x, y, 10, 600 - y);
+function shuffle(xs: number[]): number[] {
+    let out: number[] = xs;
+    let j: number;
+    for(let i = xs.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random()*i);
+        swap(out, i, j);
+    }
+    return out;
+}
+
+
+function main(): void {
+    let out: number[] = []
+    for(let i = 0; i < 600; i ++) {
+        out.push(i);
+    }
+    out = shuffle(out);
+    draw(out);
+}
+
+function draw(xs: number[]): void {
+    for(let i = 0; i < xs.length; i++) {
+        let xc = interp_3_2(0, i, xs.length, 0, 600)
+        let w = 600/xs.length
+        let yc = interp_3_2(0, xs[i], xs.length, 0, -600)
+        ctx.fillRect(xc, 600, w, yc);
+    }
+}
+
+main();
 
 
 
